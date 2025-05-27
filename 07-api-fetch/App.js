34e5a918625aafab0aa597b368a5c170ca2b82d5
Vinicius <fritzenvinicius@gmail.com>
2025-05-26 21:51:45 -0300

    Expo camera

--- 07-api-fetch/App.js
+++ 07-api-fetch/App.js
@@ -5,19 +5,89 @@ import { verifyInstallation } from 'nativewind';
 import { AuthProvider } from './contexts/AuthContext';
 import RootNavigator from './navigation/RootNavigator';
 
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
+import { ActivityIndicator, Button, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   verifyInstallation();
 
+  // return (
+    // <AuthProvider>
+    //   <RootNavigator />
+    // {/* <NavigationContainer>
+    //   <Stack.Navigator>
+    //     <Stack.Screen name="InstaClone" component={HomeScreen} />
+    //   </Stack.Navigator>
+    // </NavigationContainer> */}
+    // </AuthProvider>
+
+  // );
+  const [facing, setFacing] = useState('back');
+  const [permission, requestPermission] = useCameraPermissions();
+
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <ActivityIndicator />
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+
   return (
-    <AuthProvider>
-      <RootNavigator />
-    {/* <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="InstaClone" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer> */}
-    </AuthProvider>
+    <View style={styles.container}>
+      <CameraView style={styles.camera} facing={facing}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </CameraView>
+    </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
